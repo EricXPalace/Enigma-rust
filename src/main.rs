2fde521e5276fc71ffc2b7006b9f3589563709eb
EricXPalace <98503418+EricXPalace@users.noreply.github.com>
2023-11-02 01:53:22 +0000

    Add some code skeleton from ChatGPT.
    Suggest by Chat, it said clap is a good command line tool, here's for try out.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,50 @@
 mod enigma;
 
+use clap::{App, Arg};
+
 fn main() {
-  enigma::hello();
-  
-  let x = 5;
-  println!("x is {x}")
+    let matches = App::new("Enigma Simulator")
+        .arg(
+            Arg::with_name("dial")
+                .short("d")
+                .long("dial")
+                .multiple(true)
+                .takes_value(true)
+                .help("Set dial positions"),
+        )
+        .arg(
+            Arg::with_name("wire")
+                .short("w")
+                .long("wire")
+                .multiple(true)
+                .takes_value(true)
+                .help("Set wire configurations"),
+        )
+        .arg(Arg::with_name("INPUT").help("Input string").required(true))
+        .get_matches();
+
+    let dials: Vec<u32> = matches
+        .values_of("dial")
+        .unwrap_or_default()
+        .filter_map(|s| s.parse().ok())
+        .collect();
+
+    let wires: Vec<(char, char)> = matches
+        .values_of("wire")
+        .unwrap_or_default()
+        .filter_map(|s| {
+            if s.len() == 2 {
+                Some((s.chars().next().unwrap(), s.chars().nth(1).unwrap()))
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    let input = matches.value_of("INPUT").unwrap();
+
+    let enigma = Enigma { dials, wires };
+    let encrypted = enigma.encrypt(input);
+
+    println!("{}", encrypted);
 }
